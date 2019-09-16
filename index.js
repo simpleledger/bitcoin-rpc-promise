679191ab3d0765c46933de27197f2267285846ae
James Cramer <jcramer@simpleledger.io>
2019-09-16 12:50:06 -0400

    add timeout option

--- index.js
+++ index.js
@@ -3,17 +3,23 @@
 const RpcClientBase = require('bitcoind-rpc');
 const { promisify } = require('util');
 const URL = require('url').URL; 
-const config = { maxRetries: 5, delayMs: 100, logger: console };
+const config = { maxRetries: 5, retryDelayMs: 100, logger: console, timeoutMs: 5000 };
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-function RpcClient(url, maxRetries, retryDelayMs, logger) {
+function RpcClient(url, options) {
   if (!(this instanceof RpcClient)) return new RpcClient(url);
-  if(maxRetries)
-    config.maxRetries = maxRetries;
-  if(retryDelayMs)
-    config.delayMs = retryDelayMs;
-  if(logger)
-    config.logger = logger;
+
+  if(options) {
+    if(options.maxRetries)
+      config.maxRetries = maxRetries;
+    if(options.retryDelayMs)
+      config.retryDelayMs = retryDelayMs;
+    if(options.logger)
+      config.logger = logger;
+    if(options.timeoutMs)
+      config.timeoutMs = timeoutMs;
+  }
+
   const {
     protocol,
     username: user,
@@ -39,7 +45,13 @@ for (const key of Object.keys(RpcClientBase.callspec)) {
   RpcClient.prototype[key] = async function() {
     for(let i = 0; i < config.maxRetries; i++) {
       try {
-        const { result } = await Reflect.apply(fn, this, arguments);
+        const timeout = new Promise((resolve, reject) => {
+          let id = setTimeout(() => {
+            clearTimeout(id);
+            reject('Timed out in '+ config.timeoutMs + 'ms.')
+          }, config.timeoutMs)
+        })
+        const { result } = await Promise.race([ Reflect.apply(fn, this, arguments), timeout ]);
         return result;
       } catch(err) {
         if(i + 1 === config.maxRetries)
@@ -47,7 +59,7 @@ for (const key of Object.keys(RpcClientBase.callspec)) {
         else {
           if(config.logger)
             config.logger.log("[bitcoin-rpc-promise-retry] Retry", i + 1, key, err);
-          await sleep(config.delayMs);
+          await sleep(config.retryDelayMs);
         }
       }
     }
